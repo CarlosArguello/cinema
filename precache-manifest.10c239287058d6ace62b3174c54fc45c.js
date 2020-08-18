93df00e94ddca338b693e30799a1ef3e5cb3046e
Carlos Arguello <cararam2000@gmail.com>
2020-08-17 19:39:37 -0500

    Updates

---
+++ precache-manifest.10c239287058d6ace62b3174c54fc45c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2983b9e71008b6c75aa7acbc5b8d5e2a",
+    "url": "/cinema/index.html"
+  },
+  {
+    "revision": "04cfa0ef6449388130d8",
+    "url": "/cinema/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "565c667fce54b3a9818e",
+    "url": "/cinema/static/css/main.7425a9c0.chunk.css"
+  },
+  {
+    "revision": "04cfa0ef6449388130d8",
+    "url": "/cinema/static/js/2.d307a672.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cinema/static/js/2.d307a672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565c667fce54b3a9818e",
+    "url": "/cinema/static/js/main.9f7606dd.chunk.js"
+  },
+  {
+    "revision": "873626465c7ae165d534",
+    "url": "/cinema/static/js/runtime-main.db2dd116.js"
+  }
+]);
